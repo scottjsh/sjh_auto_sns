a378c9ee7f1c5ef266515aab70d0c28d4cc5f271
baeeol <qata163753@gmail.com>
2023-08-28 09:22:55 +0900

    feature: schedule system

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import schedule
+import time
 from services import lunchService
 
-lunchService.execute()
+schedule.every().day.at("01:00").do(lunchService.execute)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
